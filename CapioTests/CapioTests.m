a353d7cf5152b34f5cff7a31d1acc4cdf3728604
Sven A. Schmidt <sas@abstracture.de>
2011-06-30 16:33:08 +0200

    Added test for example plist

--- CapioTests/CapioTests.m
+++ CapioTests/CapioTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "CapioTests.h"
+#import "AppOverview.h"
 
 @implementation CapioTests
 
@@ -24,9 +25,17 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in CapioTests");
+- (void)test_example_plist {
+  NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
+  NSURL *url = [thisBundle URLForResource:@"app_overview" withExtension:@"plist"];
+  STAssertNotNil(url, nil);
+
+  NSArray *a = [NSArray arrayWithContentsOfURL:url];
+  STAssertNotNil(a, nil);
+  STAssertEquals(2, (int)[a count], nil);
+  NSDictionary *d1 = [a objectAtIndex:0];
+  STAssertNotNil(d1, nil);
+  STAssertEqualObjects(@"App X", [d1 objectForKey:@"appName"], nil);
 }
 
 @end
