2777d0431d01822c9d3549fa8b2204350508506c
Sven A. Schmidt <sas@abstracture.de>
2011-07-06 18:35:15 +0200

    Added test for search predicate

--- CapioTests/CapioTests.m
+++ CapioTests/CapioTests.m
@@ -32,10 +32,46 @@
 
   NSArray *a = [NSArray arrayWithContentsOfURL:url];
   STAssertNotNil(a, nil);
-  STAssertEquals(2, (int)[a count], nil);
+  STAssertEquals(7, (int)[a count], nil);
   NSDictionary *d1 = [a objectAtIndex:0];
   STAssertNotNil(d1, nil);
   STAssertEqualObjects(@"App X", [d1 objectForKey:@"appName"], nil);
 }
+
+
+- (NSArray *)readAppOverviewData {
+  NSMutableArray *apps = [NSMutableArray array];
+  
+  NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
+  NSURL *url = [thisBundle URLForResource:@"app_overview" withExtension:@"plist"];
+  
+  NSArray *data = [NSArray arrayWithContentsOfURL:url];
+  for (NSDictionary *dict in data) {
+    AppOverview *item = [[AppOverview alloc] init];
+    item.appName = [dict objectForKey:@"appName"];
+    item.appDescription = [dict objectForKey:@"appDescription"];
+    item.appOwner = [dict objectForKey:@"appOwner"];
+    item.serverCount = [dict objectForKey:@"serverCount"];
+    item.reportDate = [dict objectForKey:@"reportDate"];
+    item.ragRed = [dict objectForKey:@"ragRed"];
+    item.ragAmber = [dict objectForKey:@"ragAmber"];
+    item.ragGreen = [dict objectForKey:@"ragGreen"];
+    item.ragTotal = [dict objectForKey:@"ragTotal"];
+    [apps addObject:item];
+  }
+  return apps;
+}
+
+
+- (void)test_appfiler {
+  NSArray *apps = [self readAppOverviewData];
+  NSString *searchText = @"App";
+  NSString *asterisk = @"*";
+  NSPredicate *predicate = [NSPredicate predicateWithFormat:@"%K like %@",
+                            @"appName", [[asterisk stringByAppendingString:searchText] stringByAppendingString:asterisk]];
+  NSArray *results = [apps filteredArrayUsingPredicate:predicate];
+  STAssertEquals(3, (int)[results count], nil);
+}
+
 
 @end
