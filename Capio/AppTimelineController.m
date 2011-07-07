a95e150c81a542c6200fef6e580852bd19d6cea3
Sven A. Schmidt <sas@abstracture.de>
2011-07-07 13:37:14 +0200

    Set title

--- Capio/AppTimelineController.m
+++ Capio/AppTimelineController.m
@@ -10,13 +10,12 @@
 
 @implementation AppTimelineController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    self.title = @"Application Timeline";
+  }
+  return self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,10 +28,8 @@
 
 #pragma mark - View lifecycle
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+- (void)viewDidLoad {
+  [super viewDidLoad];
 }
 
 - (void)viewDidUnload
