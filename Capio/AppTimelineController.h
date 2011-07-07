6e1d50f6ac709edeb87719ba0743a27dfe6d3fef
Sven A. Schmidt <sas@abstracture.de>
2011-07-07 11:35:23 +0200

    Added AppTimeline controller and nib (not wired)

---
+++ Capio/AppTimelineController.h
@@ -0,0 +1,13 @@
+//
+//  AppTimelineController.h
+//  Capio
+//
+//  Created by Sven A. Schmidt on 07.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppTimelineController : UIViewController
+
+@end
