196e28053ccca4e9b88b172a3bcebbf3c2013ca0
Sven A. Schmidt <sas@abstracture.de>
2011-07-19 09:03:44 +0200

    Added ServerOverview

---
+++ Capio/ServerOverviewController.h
@@ -0,0 +1,13 @@
+//
+//  ServerOverviewController.h
+//  Capio
+//
+//  Created by Sven A. Schmidt on 19.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServerOverviewController : UIViewController
+
+@end
