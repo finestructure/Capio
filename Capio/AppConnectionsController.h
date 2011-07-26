07523775ab0143739b469057490c33e4384dd74f
Sven A. Schmidt <sas@abstracture.de>
2011-07-26 14:18:38 +0200

    Added AppConnections

---
+++ Capio/AppConnectionsController.h
@@ -0,0 +1,13 @@
+//
+//  AppConnectionsController.h
+//  Capio
+//
+//  Created by Sven A. Schmidt on 26.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppConnectionsController : UIViewController
+
+@end
