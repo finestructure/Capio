b4771c11a90e93fa22bb31be6bf20ab5922b706e
Sven A. Schmidt <sas@abstracture.de>
2011-07-27 14:27:52 +0200

    Added config view controller

---
+++ Capio/ConfigViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigViewController.h
+//  Capio
+//
+//  Created by Sven A. Schmidt on 27.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigViewController : UIViewController
+
+@end
