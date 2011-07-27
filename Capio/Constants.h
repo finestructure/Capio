c4bbe7a6be69d4e3e981f27db611d517121c213c
Sven A. Schmidt <sas@abstracture.de>
2011-07-27 17:19:29 +0200

    Added Constants

---
+++ Capio/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Capio
+//
+//  Created by Sven A. Schmidt on 27.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kCouchServiceName;
+extern NSString * const kCouchServiceUrl;
+
---
+++ Capio/Constants.m
@@ -0,0 +1,12 @@
+//
+//  Constants.m
+//  Capio
+//
+//  Created by Sven A. Schmidt on 27.07.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString * const kCouchServiceName = @"CouchServiceName";
+NSString * const kCouchServiceUrl = @"CouchServiceUrl";
