1db7a5695fcfae1b2d12deb09cffa7338614ee91
Sven A. Schmidt <sas@abstracture.de>
2011-07-28 16:47:53 +0200

    Don't show server overview when doc is nil

--- Capio/AppOverviewController.m
+++ Capio/AppOverviewController.m
@@ -110,10 +110,11 @@
     NSString *server = [self.detailItem.serverList objectAtIndex:0];
     NSDate *asof = self.detailItem.reportDate;
     NSDictionary *doc = [[DataStore sharedDataStore] fetchDocument:server forDate:asof];
-        
-    ServerOverviewController *vc = [[ServerOverviewController alloc] initWithNibName:@"ServerOverview" bundle:nil];
-    vc.detailItem = doc;
-    [self.navigationController pushViewController:vc animated:YES];
+    if (doc != nil) {
+      ServerOverviewController *vc = [[ServerOverviewController alloc] initWithNibName:@"ServerOverview" bundle:nil];
+      vc.detailItem = doc;
+      [self.navigationController pushViewController:vc animated:YES];
+    }
   } else if (count > 1) {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Not Implemented" message:@"Display of multiple servers not implemented yet" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [alert show];
